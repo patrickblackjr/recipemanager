38d95a170d43a58bebad6960fbcf362ebb404de9
Patrick Black <pj@perfectimprints.com>
2022-01-02 02:58:06 -0600

    dev init

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from 'vue'
+import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
-createApp(App).mount("#app");
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { Vue } from 'vue/types/vue'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+createApp(App).mount('#app')
