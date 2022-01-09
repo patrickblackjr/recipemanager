f6244ae6f6c0c037275d23f52256316d60f93cf5
Patrick Black <info@patrickblackjr.com>
2022-01-09 16:59:54 -0600

    add apollo and prisma

---
+++ server/apollo.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  client: {
+    service: {
+      name: 'RecipeManager',
+      url: 'http://localhost:4000/graphql',
+    },
+  },
+}
--- server/index.js
+++ server/index.js
@@ -2,87 +2,33 @@ import 'dotenv/config'
 import cors from 'cors'
 import express from 'express'
 import { ApolloServer, gql } from 'apollo-server-express'
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+const fs = require('fs')
+const path = require('path')
 
 const app = express()
 
+// app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
-
-const schema = gql`
-  type Query {
-    users: [User!]
-    me: User
-    user(id: ID!): User
-
-    messages: [Message!]!
-    message(id: ID!): Message!
-  }
-  type User {
-    id: ID!
-    username: String!
-  }
-  type Message {
-    id: ID!
-    text: String!
-    user: User!
-  }
-`
-
-let users = {
-  1: {
-    id: '1',
-    username: 'Robin Wieruch',
-  },
-  2: {
-    id: '2',
-    username: 'Dave Davids',
-  },
-}
-
-const me = users[1]
-let messages = {
-  1: {
-    id: '1',
-    text: 'Hello World',
-  },
-  2: {
-    id: '2',
-    text: 'By World',
-  },
-}
 
 const resolvers = {
   Query: {
-    users: () => {
-      return Object.values(users)
-    },
-    user: (parent, { id }) => {
-      return users[id]
-    },
-    me: (parent, args, { me }) => {
-      return me
-    },
-    messages: () => {
-      return Object.values(messages)
-    },
-    message: (parent, { id }) => {
-      return messages[id]
-    },
-  },
-  User: {
-    username: (user) => `${user.firstname} ${user.lastname}`,
-  },
-  Message: {
-    user: (parent, args, { me }) => {
-      return me
+    recipe: async (obj, args, context, info) => {
+      return context.prisma.recipes.findMany()
     },
   },
 }
 
 const server = new ApolloServer({
-  typeDefs: schema,
+  apollo: {
+    key: process.env.APOLLO_KEY,
+  },
+  typeDefs: fs.readFileSync(path.join(__dirname, 'schema.graphql'), 'utf8'),
   resolvers,
   context: {
-    me: users[1],
+    prisma,
   },
 })
 
