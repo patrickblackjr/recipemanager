1c84434a015a7a58d41e4bd4005607efaa0e9a62
Patrick Black <info@patrickblackjr.com>
2022-01-14 15:49:34 -0600

    fix(deps): update eslint rules

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -14,12 +14,7 @@ module.exports = {
     'vue/component-name-in-template-casing': ['error', 'PascalCase'],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'vue/multi-word-component-names': [
-      'error',
-      {
-        ignores: ['index'],
-      },
-    ],
+    'vue/multi-word-component-names': 'off',
   },
   globals: {
     $nuxt: true,
